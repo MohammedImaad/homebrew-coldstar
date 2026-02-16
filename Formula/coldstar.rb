ea27246381a55ebdf44b639b8bff00432abbfb10
MohammedImaad <thouheed.imaad@gmail.com>
2026-02-16 11:09:39 +0000

    Fixed for devsyrem

--- Formula/coldstar.rb
+++ Formula/coldstar.rb
@@ -1,8 +1,8 @@
 class Coldstar < Formula
   desc "Coldstar CLI"
-  homepage "https://github.com/MohammedImaad/coldstar"
-  url "https://github.com/MohammedImaad/coldstar/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b81bfd327a9b0355347d0926a42f667857a1bc92d85ba93435154fdd66b84c16"
+  homepage "https://github.com/devsyrem/coldstar"
+  url "https://github.com/devsyrem/homebrew-coldstar/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "4738fcade68f2158a8bb541eb1a27598d0f764a0d3a8a2400916ca5ce9dfd9b7"
   license "MIT"
 
   depends_on "python@3.11"
