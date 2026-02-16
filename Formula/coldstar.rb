0a13d19abfb72c8124d59f75428442f7e0800ab1
MohammedImaad <thouheed.imaad@gmail.com>
2026-02-16 10:01:14 +0000

    RHA Fix

--- Formula/coldstar.rb
+++ Formula/coldstar.rb
@@ -2,7 +2,7 @@ class Coldstar < Formula
   desc "Coldstar CLI"
   homepage "https://github.com/MohammedImaad/coldstar"
   url "https://github.com/MohammedImaad/coldstar/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "3014f77617417dd365c3b8d0a94930f209b2fcc2b7e5ebbf3fc5aa2701036542"
+  sha256 "b81bfd327a9b0355347d0926a42f667857a1bc92d85ba93435154fdd66b84c16"
   license "MIT"
 
   depends_on "python@3.11"
